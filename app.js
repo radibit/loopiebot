8f8bfd632fcf25a5e86f07f3f2a9d8d3d713d91e
radibit <radimir.bitsov@gmail.com>
2016-02-18 23:52:25 +0100

    sample configuration

--- app.js
+++ app.js
@@ -3,11 +3,20 @@
 var express    = require( 'express' );
 var SlackBot   = require( 'slackbots' );
 var bodyParser = require( 'body-parser' );
+var request    = require( 'request' );
 
 var loopiebot  = require( './loopiebot' );
 
 var app  = express();
 var port = process.env.PORT || 3000;
+
+var options = {
+  url     : process.env.LOOPIE_URI,
+  headers : {
+    'X-Loopline-Client' : process.env.LOOPIE_X_CLIENT,
+    'Authorization'     : process.env.LOOPIE_AUTH
+  }
+};
 
 // body parser middleware
 app.use( bodyParser.urlencoded( {
@@ -15,14 +24,22 @@ app.use( bodyParser.urlencoded( {
 );
 
 // test route
-app.get( '/', function (req, res) {
-  res.status( 200 ).send( 'Hello loopies!' )
+app.get( '/', function ( req, res ) {
+  request( options, function( error, response, body ) {
+    if ( !error && response.statusCode == 200 ) {
+      var info = JSON.parse( body );
+      console.log( info );
+      console.log( body );
+
+      res.status( 200 ).send( body );
+    }
+  } );
 } );
 
 app.post( '/loopie', loopiebot );
 
 // error handler
-app.use(function ( err, req, res, next ) {
+app.use( function ( err, req, res, next ) {
   console.error( err.stack );
 
   res.status( 400 ).send( err.message );
